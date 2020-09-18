496d2b50535c0356f03e40ae102247f311a82185
Azim Akmatov <azim.akmatov1@gmail.com>
2020-09-18 14:22:28 +0000

    more accurate filtering

--- routes/books.js
+++ routes/books.js
@@ -11,19 +11,19 @@ router.get('/books', (req, res) => {
       var booksFiltered = [];
       if (query.book) {
         for (book in books) {
-          if (books[book].title.toLowerCase().startsWith(query.book.toLowerCase())) {
+          if (books[book].title.toLowerCase().includes(query.book.toLowerCase())) {
             booksFiltered.push(books[book]);
           }
         }
       } else if (query.subject) {
         for (book in books) {
-          if (books[book].subject.toLowerCase().startsWith(query.subject.toLowerCase()) ) {
+          if (books[book].subject.toLowerCase().includes(query.subject.toLowerCase()) ) {
             booksFiltered.push(books[book]);
           }
         }
       } else if (query.teacher) {
         for (book in books) {
-          if (books[book].teacher.toLowerCase().startsWith(query.teacher.toLowerCase())) {
+          if (books[book].teacher.toLowerCase().includes(query.teacher.toLowerCase())) {
             booksFiltered.push(books[book]);
           }
         }
