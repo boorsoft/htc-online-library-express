29a7f3b772f877ec6d25d3fcf3c96c55f2fda271
Azim Akmatov <azim.akmatov1@gmail.com>
2020-09-29 07:44:10 +0000

    post & put to books

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 const booksRoutes = require('./routes/books');
+
+const Book = require('./models/bookModel');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -12,5 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use('/api', booksRoutes);
+app.get('/admin/books', (req, res) => {
+  Book.findAll().then(books => {
+    
+  })
+  res.sendFile(path.join(__dirname, 'public/admin.html'));
+});
 
 module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -9,19 +9,19 @@ router.get('/books', (req, res) => {
 
     function filterBooks(query) {
       var booksFiltered = [];
-      if (query.book) {
+      if (query.book && query.book !== '') {
         for (book in books) {
           if (books[book].title.toLowerCase().includes(query.book.toLowerCase())) {
             booksFiltered.push(books[book]);
           }
         }
-      } else if (query.subject) {
+      } else if (query.subject && query.subject !== '') {
         for (book in books) {
           if (books[book].subject.toLowerCase().includes(query.subject.toLowerCase()) ) {
             booksFiltered.push(books[book]);
           }
         }
-      } else if (query.teacher) {
+      } else if (query.teacher && query.teacher !== '') {
         for (book in books) {
           if (books[book].teacher.toLowerCase().includes(query.teacher.toLowerCase())) {
             booksFiltered.push(books[book]);
@@ -37,6 +37,28 @@ router.get('/books', (req, res) => {
     res.status(200).json(filterBooks(req.query));
   });
 
+});
+
+router.post('/books', (req, res) => {
+  const book = Book.create({
+    title: req.body.title,
+    author: req.body.author,
+    teacher: req.body.teacher,
+    sujbject: req.body.subject,
+    filename: req.body.filename
+  });
+  res.status(200).json({"message": "Book created!"});
+});
+
+router.put('/books', (req, res) => {
+  const book = Book.create({
+    title: req.body.title,
+    author: req.body.author,
+    teacher: req.body.teacher,
+    sujbject: req.body.subject,
+    filename: req.body.filename
+  });
+  res.status(200).json({"message": "Book modified!"});
 });
 
 module.exports = router;
