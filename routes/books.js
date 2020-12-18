e27482c16b586319131fdffaaf98de14ebbd115d
Azim Akmatov <azim.akmatov1@gmail.com>
2020-12-18 14:04:26 +0000

    UPDATE: API data update requests

--- routes/books.js
+++ routes/books.js
@@ -5,26 +5,32 @@ const Book = require('../models/bookModel');
 
 router.get('/books', (req, res) => {
 
-  Book.findAll().then(books => {
+  Book.findAll().then((books) => {
 
     function filterBooks(query) {
       var booksFiltered = [];
       if (query.book && query.book !== '') {
         for (book in books) {
-          if (books[book].title.toLowerCase().includes(query.book.toLowerCase())) {
-            booksFiltered.push(books[book]);
+          if (books[book].title) {
+            if (books[book].title.toLowerCase().includes(query.book.toLowerCase())) {
+              booksFiltered.push(books[book]);
+            }
           }
         }
       } else if (query.subject && query.subject !== '') {
         for (book in books) {
-          if (books[book].subject.toLowerCase().includes(query.subject.toLowerCase()) ) {
-            booksFiltered.push(books[book]);
+          if (books[book].subject) {
+            if (books[book].subject.toLowerCase().includes(query.subject.toLowerCase())) {
+              booksFiltered.push(books[book]);
+            }
           }
         }
       } else if (query.teacher && query.teacher !== '') {
         for (book in books) {
-          if (books[book].teacher.toLowerCase().includes(query.teacher.toLowerCase())) {
-            booksFiltered.push(books[book]);
+          if (books[book].teacher) {
+            if (books[book].teacher.toLowerCase().includes(query.teacher.toLowerCase())) {
+              booksFiltered.push(books[book]);
+            }
           }
         }
       } else {
@@ -35,9 +41,23 @@ router.get('/books', (req, res) => {
     }
 
     res.status(200).json(filterBooks(req.query));
-  });
+  }).catch((err) => console.log(err));
 
 });
+
+router.get('/books/:id', (req, res) => {
+  const book = Book.findOne({where: {"book_id": req.params.id}}).then((data) => {
+    res.status(200).json({
+      "book_id": data.book_id,
+      "title": data.title,
+      "author": data.author,
+      "teacher": data.teacher,
+      "subject": data.subject,
+      "filename": data.filename
+    })
+  })
+  
+})
 
 router.post('/books', (req, res) => {
   const book = Book.create({
@@ -50,14 +70,15 @@ router.post('/books', (req, res) => {
   res.status(200).json({"message": "Book created!"});
 });
 
-router.put('/books', (req, res) => {
-  const book = Book.create({
+router.put('/books/:id', (req, res) => {
+  const book = Book.update({
     title: req.body.title,
     author: req.body.author,
     teacher: req.body.teacher,
     sujbject: req.body.subject,
     filename: req.body.filename
-  });
+  }, {where: {book_id: req.params.id}});
+
   res.status(200).json({"message": "Book modified!"});
 });
 
