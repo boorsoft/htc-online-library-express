3a8b39952aa82d8c1a75006b70b90518c3b5734f
akmatoff <azim.akmatov1@gmail.com>
2021-03-30 11:14:47 +0600

    database update

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'db.sqlite'
+const db = new Sequelize('database', process.env['DB_USER'], process.env['DB_PASSWORD'], {
+  host: 'ec2-3-95-85-91.compute-1.amazonaws.com',
+  dialect: 'postgres'
 });
 
 module.exports = db;
